00297f598dea2f2c55b32598abc38d82f3789702
eda716 <re25304008@ga.ttc.ac.jp>
2025-12-04 17:22:28 +0900

    oi

---
+++ WEB-INF/src/GitInputServlet.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class GitInputServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        request.setCharacterEncoding("UTF-8"); // 日本語対応
+        String lastName = request.getParameter("lastName");
+        String firstName = request.getParameter("firstName");
+
+        response.setContentType("text/html; charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        out.println("<html><body>");
+        out.println("<h2>入力された内容</h2>");
+        out.println("姓: " + lastName + "<br>");
+        out.println("名: " + firstName + "<br>");
+        out.println("</body></html>");
+    }
+}
--- gitinpt.jsp
+++ gitinpt.jsp
@@ -1,12 +1,15 @@
 <%@ page contentType="text/html; charset=UTF-8" %>
 <html>
-<head><title>名前入力</title></head>
+<head>
+    <title>名前入力フォーム</title>
+</head>
 <body>
     <h2>名前を入力してください</h2>
-    <form action="GitInputServlet" method="get"></form>
-        姓: <input type="text" name="last"><br>
-        名: <input type="text" name="first"><br>
+    <form action="GitInputServlet" method="post">
+        姓: <input type="text" name="lastName"><br>
+        名: <input type="text" name="firstName"><br>
         <input type="submit" value="送信">
     </form>
 </body>
 </html>
+
